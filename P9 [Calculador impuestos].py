deb9bb075c9ca62478bc04bd0d1933e8fcf9522e
Nestor Morales <143459775+NestorMorales271@users.noreply.github.com>
2025-03-14 00:10:27 +0000

    Fixed P9 details

--- P9 [Calculador impuestos].py
+++ P9 [Calculador impuestos].py
@@ -1,9 +1,9 @@
-income = float(input("impuestos"))
+income = float(input("Ingresa tus ingresos anuales: "))
 
 if income < 85528:
 	tax = income * 0.18 - 556.02
 # Write the rest of your code here.
 
 tax = round(tax, 0)
-print("impuestos", tax, " pesos.")
+print("Tus impuestos son: ", tax, " pesos.")
  
