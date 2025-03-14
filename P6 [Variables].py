13965a1e50796cb14bf2fbe66d01a9592bfb89cf
Nestor Morales <143459775+NestorMorales271@users.noreply.github.com>
2025-03-14 00:00:45 +0000

    Fixed details

--- P6 [Variables].py
+++ P6 [Variables].py
@@ -1,5 +1,5 @@
 var = 1
 account_balance = 1000.0
 client_name = 'John Doe'
-print("No. Cliente: ",var,"/nEstado de cuenta: ", account_balance,"/nNombre del cliente: ", client_name)
+print("No. Cliente: ",var,"\nEstado de cuenta: ", account_balance,"\nNombre del cliente: ", client_name)
 print(var)
---
+++ P8 [Operadores String].py
@@ -0,0 +1,4 @@
+name = input("Ingresa tu primer nombre: ")
+lastname = input("\nIngresa tu primer apeillido: ")
+print("\nGracias.")
+print("\nTu nombre: " + name + " " + lastname + ".")
